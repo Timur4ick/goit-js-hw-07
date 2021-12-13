496be83bdb50d8012300db5b0a0e451a5be17f0a
Timur4ick <79656475+Timur4ick@users.noreply.github.com>
2021-12-13 23:43:59 +0200

    link-script

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallary = document.querySelector(".gallery");
+
+const cardsGallery = imgCards(galleryItems);
+
+function imgCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join(" ");
+}
+gallary.insertAdjacentHTML("beforeend", cardsGallery);
+
+gallary.addEventListener("click", clickBtn);
+
+function clickBtn(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+}
+let lightbox = new SimpleLightbox(".gallery__link", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
